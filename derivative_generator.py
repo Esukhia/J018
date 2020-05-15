77c00cc3c7861881f2ce4018dcad55c68b422d7f
ngawangtrinley <ngawangtrinley@gmail.com>
2020-05-16 01:13:26 +0800

    fix

--- derivative_generator.py
+++ derivative_generator.py
@@ -20,11 +20,25 @@ formats = ['unicode', 'wylie']
 def derive(project, type, format):
     print(f'Deriving {project}-{type}-{format}...')
     path = Path(f'{project}-suggestions-unicode')
+
     for file in path.iterdir():
         text = file.read_text(encoding='utf-8')
 
-        text = re.sub('\((.+?),.+?\)','\g<1>', text) if type == 'diplomatic' else text
-        new_text = pyewts().toWylie(text) if format == 'wylie' else text
+        if type == 'suggestions' and format == 'wylie':
+            new_text = pyewts().toWylie(text)
+            new_text = re.sub(' ?\[\(\]',' (', new_text)
+            new_text = re.sub(' ?\[\)\]',') ', new_text)
+            new_text = re.sub(' ?\[,\]',',', new_text)
+
+        elif type == 'diplomatic' and format == 'unicode':
+            new_text = re.sub('\((.+?),.+?\)','\g<1>', text)
+
+        elif type == 'diplomatic' and format == 'wylie':
+            new_text = re.sub('\((.+?),.+?\)','\g<1>', text)
+            new_text = pyewts().toWylie(new_text)
+        else:
+            new_text = 'wtf'
+
         new_path = Path(f'{project}-{type}-{format}')
         new_path.mkdir(parents=True, exist_ok=True)
         new_path = new_path / file.name
