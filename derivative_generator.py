4518d3839f08225e930ba2b17df09bcb82e6d45e
ngawangtrinley <ngawangtrinley@gmail.com>
2020-05-15 15:04:48 +0800

    derivatives

--- derivative_generator.py
+++ derivative_generator.py
@@ -7,6 +7,7 @@ and generates following derivatives for Gregory:
 '''
 from pathlib import Path
 from pyewts import pyewts
+import re
 
 # "<project>-<type>-<format>"
 # "J018-diplomatic-unicode"
@@ -17,9 +18,12 @@ formats = ['unicode', 'wylie']
 
 
 def derive(project, type, format):
+    print(f'Deriving {project}-{type}-{format}...')
     path = Path(f'{project}-suggestions-unicode')
     for file in path.iterdir():
         text = file.read_text(encoding='utf-8')
+
+        text = re.sub('\((.+?),.+?\)','\g<1>', text) if type == 'diplomatic' else text
         new_text = pyewts().toWylie(text) if format == 'wylie' else text
         new_path = Path(f'{project}-{type}-{format}')
         new_path.mkdir(parents=True, exist_ok=True)
@@ -30,4 +34,6 @@ def derive(project, type, format):
 # suggestions in wylie
 derive(project, types[0], formats[1])
 # diplomatic in unicode
+derive(project, types[1], formats[0])
 # diplomatic in wylie
+derive(project, types[1], formats[1])
